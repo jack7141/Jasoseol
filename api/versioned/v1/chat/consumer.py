3e98d60821ac79ebb6a99d1d9ae92c3ef70d3e7d
황광회 <jack7141@nate.com>
2024-08-23 00:53:24 +0900

    Add:  채팅방 유저 인입시 실시간 count 추가

--- api/versioned/v1/chat/consumer.py
+++ api/versioned/v1/chat/consumer.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+import arrow
 
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
@@ -7,10 +8,16 @@ import json
 
 from django.core.cache import cache
 
-from api.bases.chat.models import ChatRoom
+from api.bases.chat.models import ChatRoom, ChatRoomParticipant
 from common.exceptions import ExpiredApiCacheData
 from common.utils import CacheDataManager
 from api.bases.user.models import User
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
@@ -25,11 +32,17 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             await self.accept()
 
             self.user = await self.get_user_or_404(user_id)
+
+            await self.add_user_to_room(self.room_id, self.user.id)
+
+            connected_users_count = await self.get_connected_users_count(self.room_id)
+
             await self.channel_layer.group_send(
                 self.group_name,
                 {
                     'type': 'user_join',
                     'username': self.user.username,
+                    'connected_users_count': connected_users_count
                 }
             )
         except ValueError as e:
@@ -40,15 +53,22 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         try:
             group_name = self.get_group_name(self.room_id)
             await self.channel_layer.group_discard(group_name, self.channel_name)
+
+            await self.remove_user_from_room(self.room_id, self.user.id)
+
+            connected_users_count = await self.get_connected_users_count(self.room_id)
+
             await self.channel_layer.group_send(
                 self.group_name,
                 {
                     'type': 'user_leave',
                     'username': self.user.username,
+                    'connected_users_count': connected_users_count
                 }
             )
         except Exception as e:
-            pass
+            logger.error(f'Error in disconnect: {str(e)}')
+
 
 
     async def receive_json(self, content):
@@ -82,16 +102,20 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
 
     async def user_join(self, event):
         username = event['username']
+        connected_users_count = event['connected_users_count']
         await self.send_json({
             "type": "user_join",
-            "message": f"{username} has joined the chat."
+            "message": f"{username} has joined the chat.",
+            "connected_users_count": connected_users_count
         })
 
     async def user_leave(self, event):
         username = event['username']
+        connected_users_count = event['connected_users_count']
         await self.send_json({
             "type": "user_leave",
-            "message": f"{username} has left the chat."
+            "message": f"{username} has left the chat.",
+            "connected_users_count": connected_users_count
         })
 
     async def recieve_group_message(self, event):
@@ -115,3 +139,22 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             return User.objects.get(id=user_id)
         except User.DoesNotExist:
             raise ValueError('User does not exist.')
+
+
+    @database_sync_to_async
+    def add_user_to_room(self, room_id, user_id):
+        room = ChatRoom.objects.get(id=room_id)
+        ChatRoomParticipant.objects.update_or_create(
+            user_id=user_id,
+            chat_room=room,
+            defaults={'joined_at': arrow.now("Asia/Seoul").datetime}
+        )
+
+    @database_sync_to_async
+    def get_connected_users_count(self, room_id):
+        thirty_minutes_ago = arrow.now("Asia/Seoul").shift(minutes=-30).datetime
+        return ChatRoomParticipant.objects.filter(chat_room=room_id, joined_at__gte=thirty_minutes_ago).count()
+
+    @database_sync_to_async
+    def remove_user_from_room(self, room_id, user_id):
+        ChatRoomParticipant.objects.filter(user=user_id, chat_room=room_id).delete()
